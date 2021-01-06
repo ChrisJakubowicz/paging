8a0973cf12705e99e6d5c459630ab918e42eac76
Chris Jakubowicz <chris.jakubowicz@ministrybrands.com>
2021-01-06 16:14:39 -0500

    Add PagingSource

---
+++ app/src/main/java/com/experiment/android/prototype/paging/data/GithubPagingSource.kt
@@ -0,0 +1,34 @@
+package com.experiment.android.prototype.paging.data
+
+import androidx.paging.PagingSource
+import com.experiment.android.prototype.paging.api.GithubService
+import com.experiment.android.prototype.paging.api.IN_QUALIFIER
+import com.experiment.android.prototype.paging.model.Repo
+import retrofit2.HttpException
+import java.io.IOException
+
+// GitHub page API is 1 based: https://developer.github.com/v3/#pagination
+private const val GITHUB_STARTING_PAGE_INDEX = 1
+
+class GithubPagingSource(
+        private val service: GithubService,
+        private val query: String
+) : PagingSource<Int, Repo>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Repo> {
+        val position = params.key ?: GITHUB_STARTING_PAGE_INDEX
+        val apiQuery = query + IN_QUALIFIER
+        return try {
+            val response = service.searchRepos(apiQuery, position, params.loadSize)
+            val repos = response.items
+            LoadResult.Page(
+                    data = repos,
+                    prevKey = if (position == GITHUB_STARTING_PAGE_INDEX) null else position - 1,
+                    nextKey = if (repos.isEmpty()) null else position + 1
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
+    }
+}
